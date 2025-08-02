ca70f16d269a6bd3f66c10db8747a2c48ffcb861
eswar <eswarananth321@gmail.com>
2025-08-02 19:15:18 +0530

    db changes

--- config/connectDatabase.js
+++ config/connectDatabase.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
 export function connectDataBase(){
-    mongoose.connect(process.env.DEV_CNN_STR);
+    mongoose.connect(process.env.PROD_CNN_STR, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(suc => {
+        console.log('Connected');
+    }).catch(err => {
+        console.log(err);
+    });
 }
